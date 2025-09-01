ceeccd1341d46a2574766c14e4b2eeeb7343942a
Huy Phạm Duy <24410047@ms.uit.edu.vn>
2025-09-02 02:46:10 +0700

    feat(aiAgent): add basic AI service classes and Dockerfile
    - Introduce AiService interface and related classes: ChatController, ChatRequest,
      LLMAiService, SecurityConfig
    - Create Dockerfile for aiAgent-service using eclipse-temurin:21-jre-alpine base image
    - Copy payment jar file and set entry point to run the jar

--- aiAgent-service/src/main/java/com/pdh/ai/AiAgentServiceApplication.java
+++ aiAgent-service/src/main/java/com/pdh/ai/AiAgentServiceApplication.java
@@ -1,19 +1,15 @@
-package com.pdh.backoffice;
+package com.pdh.ai;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@Slf4j
-public class BackOfficeApplication {
+public class AiAgentServiceApplication {
 
     public static void main(String[] args) {
-
-        SpringApplication.run(BackOfficeApplication.class, args);
-
+        SpringApplication.run(AiAgentServiceApplication.class, args);
     }
+
 }
---
+++ aiAgent-service/src/main/java/com/pdh/ai/SecurityConfig.java
@@ -0,0 +1,91 @@
+package com.pdh.ai;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.modelcontextprotocol.client.transport.WebFluxSseClientTransport;
+
+import org.springframework.ai.mcp.client.autoconfigure.NamedClientMcpTransport;
+import org.springframework.ai.mcp.client.autoconfigure.properties.McpClientCommonProperties;
+import org.springframework.ai.mcp.client.autoconfigure.properties.McpSseClientProperties;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+@EnableConfigurationProperties({ McpSseClientProperties.class })
+public class SecurityConfig{
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth ->
+                        auth.anyRequest().permitAll()
+                )
+                .oauth2Client(Customizer.withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .csrf(CsrfConfigurer::disable)
+                .build();
+    }
+
+    @Bean
+    public List<NamedClientMcpTransport> webFluxClientTransports(McpSseClientProperties sseProperties,
+                                                                 ObjectProvider<WebClient.Builder> webClientBuilderProvider,
+                                                                 ObjectProvider<ObjectMapper> objectMapperProvider,
+                                                                 OAuth2AuthorizedClientManager authorizedClientManager) {
+
+        List<NamedClientMcpTransport> sseTransports = new ArrayList<>();
+
+        var webClientBuilderTemplate = webClientBuilderProvider.getIfAvailable(WebClient::builder);
+        var objectMapper = objectMapperProvider.getIfAvailable(ObjectMapper::new);
+
+        for (Map.Entry<String, McpSseClientProperties.SseParameters> serverParameters : sseProperties.getConnections().entrySet()) {
+            ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
+                    new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+            oauth2Client.setDefaultClientRegistrationId("my-client1");
+
+            var webClientBuilder = webClientBuilderTemplate.clone().baseUrl(serverParameters.getValue().url());
+            String sseEndpoint = serverParameters.getValue().sseEndpoint() != null
+                    ? serverParameters.getValue().sseEndpoint() : "/sse";
+            var transport = WebFluxSseClientTransport.builder(webClientBuilder.apply(oauth2Client.oauth2Configuration()))
+                    .sseEndpoint(sseEndpoint)
+                    .objectMapper(objectMapper)
+                    .build();
+            sseTransports.add(new NamedClientMcpTransport(serverParameters.getKey(), transport));
+        }
+
+        return sseTransports;
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientService authorizedClientService) {
+
+        OAuth2AuthorizedClientProvider authorizedClientProvider =
+                OAuth2AuthorizedClientProviderBuilder.builder()
+                        .clientCredentials()
+                        .build();
+
+        AuthorizedClientServiceOAuth2AuthorizedClientManager authorizedClientManager =
+                new AuthorizedClientServiceOAuth2AuthorizedClientManager(
+                        clientRegistrationRepository, authorizedClientService);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return authorizedClientManager;
+    }
+}
