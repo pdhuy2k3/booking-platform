a052b179b8885a6eab77cd13dbdf5f72ee8691a5
Huy Phạm Duy <24410047@ms.uit.edu.vn>
2025-09-14 12:17:16 +0700

    fix(storefront-bff): Add security dependency and fix StoreFrontApplication annotations

--- storefront-bff/src/main/java/com/pdh/storefront/StoreFrontApplication.java
+++ storefront-bff/src/main/java/com/pdh/storefront/StoreFrontApplication.java
@@ -4,11 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableWebFluxSecurity
 @Slf4j
 public class StoreFrontApplication {
 
