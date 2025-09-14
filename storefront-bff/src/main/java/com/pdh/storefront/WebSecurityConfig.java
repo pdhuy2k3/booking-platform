a9d103119f630f79077f248d52cc8f7a4814f25b
Huy Phạm Duy <24410047@ms.uit.edu.vn>
2025-09-14 12:15:55 +0700

    fix(storefront-bff): Recreate WebSecurityConfig.java with API Gateway configuration

--- storefront-bff/src/main/java/com/pdh/storefront/WebSecurityConfig.java
+++ storefront-bff/src/main/java/com/pdh/storefront/WebSecurityConfig.java
@@ -1,4 +1,5 @@
 package com.pdh.storefront;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
